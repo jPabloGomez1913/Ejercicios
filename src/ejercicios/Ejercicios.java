e453fa207793bac6827e2f3a979020dbdd77e541
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-07-13 18:07:13 -0500

    pedir numeros hasta que se ingrese uno negativo

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -8,7 +8,8 @@ public class Ejercicios {
 
     public static void main(String[] args) {
         //mayor();
-        mayor3Num();
+        //mayor3Num();
+        sumBucle();
     }
 
     static public void mayor() {
@@ -37,39 +38,55 @@ public class Ejercicios {
 
     static public void mayor3Num() {
         /*Mayor y menor de 3 numeros ingresados*/
-        int num1, num2, num3, mayor, menor=0;
+        int num1, num2, num3, mayor, menor = 0;
         System.out.println("Ingrese un numero");
         num1 = entrada.nextInt();
         System.out.println("Ingrese un numero");
         num2 = entrada.nextInt();
         System.out.println("Ingrese un numero");
         num3 = entrada.nextInt();
-        if(num1>num2 && num1>num3){
-            mayor=num1;
-            if(num2>num3){
-                menor=num3;
+        if (num1 > num2 && num1 > num3) {
+            mayor = num1;
+            if (num2 > num3) {
+                menor = num3;
+            } else {
+                menor = num2;
             }
-            else{
-                menor=num2;
+        } else if (num2 > num3) {
+            mayor = num2;
+            if (num3 > num1) {
+                menor = num1;
             }
-        }
-        else if (num2>num3){
-            mayor=num2;
-            if (num3>num1) {
-                menor=num1;
-            }
-        }
-        else {
-            mayor=num3;
-            if (num2>num1) {
-                menor=num1;
-            }
-            else {
-                menor=num2;
+        } else {
+            mayor = num3;
+            if (num2 > num1) {
+                menor = num1;
+            } else {
+                menor = num2;
             }
         }
-        System.out.println("El numero mayor es: "+mayor);
-        System.out.println("El numero menor es: "+menor);
+        System.out.println("El numero mayor es: " + mayor);
+        System.out.println("El numero menor es: " + menor);
     }
 
+    static public void sumBucle() {
+        /* pedir numeros hasta que se ingrese uno negativo y 
+       mostrar la suma y promedio de los ingreados*/
+        int num, suma = 0, prom, cont = 0;
+        
+        
+        
+        do {
+            System.out.println("Ingrese un numero: ");
+            num=entrada.nextInt();
+            if (num<0) {
+                break;
+            }
+            suma+=num;
+            cont+=1;
+        } while (num>0);
+        prom=suma/cont;
+        System.out.println("La suma es: "+suma);
+        System.out.println("El promedio es: "+prom);
+    }
 }

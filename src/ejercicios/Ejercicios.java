1cb151de1374eab816dd0682a5cec6dd7807d9dd
juan pablo gomez <Juangomezramirez527@gmil.com>
2022-07-13 16:56:45 -0500

    Ejercicio del num mayor de 2 numeros

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package ejercicios;
 
-/**
- *
- * @author juang
- */
+import java.util.Scanner;
+
 public class Ejercicios {
 
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner entrada = new Scanner(System.in);
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        mayor();
     }
-    
-}
+
+    static public void mayor() {
+        /*Programa que pida 2 numeros y que nos diga cual es el mayor,
+        el menor y si son iguales */
+        int num1, num2, mayor, menor;
+        System.out.println("Ingrese un numero");
+        num1 = entrada.nextInt();
+        System.out.println("Ingrese un numero");
+        num2 = entrada.nextInt();
+        if (num1 > num2) {
+            mayor = num1;
+            menor = num2;
+            System.out.println("El mayor es " + mayor);
+            System.out.println("El menor es " + menor);
+        }
+        else if(num2>num1){
+                mayor=num2;
+                menor=num1;
+                System.out.println("El mayor es "+mayor);
+                System.out.println("El menor es "+menor);
+        }
+        else{
+            System.out.println("los numeros son iguales ");
+            
+        }
+    }
+            
+
+        
+    }
